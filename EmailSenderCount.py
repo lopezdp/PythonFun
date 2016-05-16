51c0f0b3934c4277d4752370090f83ea60ac6049
David Lopez <lopezdp@users.noreply.github.com>
2016-05-16 16:46:24 -0400

    Update EmailSenderCount.py
    getting closer with "tracer rounds" now printing compiled list of email addresses each with count from inside of dict()

--- EmailSenderCount.py
+++ EmailSenderCount.py
@@ -23,12 +23,12 @@ for line in fh:
     line = line.rstrip()
     if not line.startswith('From '): continue
     words = line.split()
-    print words
-    for word in words:
-        if word not in counts:
-            counts[word] = 1
+    #print words
+    for word in words[1]:
+        if words[1] not in counts :
+            counts[words[1]] = 1
         else:
-            counts[word] += 1
+            counts[words[1]] += 1
             
 for key in counts:
         print key, counts[key]

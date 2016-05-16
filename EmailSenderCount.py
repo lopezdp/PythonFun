d9292da303dfa4551a685761211affb1db1ad20c
David Lopez <lopezdp@users.noreply.github.com>
2016-05-16 01:31:56 -0400

    Create EmailSenderCount.py
    the program reads through a txt file and sorts the email addresses into a dictionary and uses a maximum loop to find the most prolific sender of emails

---
+++ EmailSenderCount.py
@@ -0,0 +1,34 @@
+#9.4 Write a program to read through the mbox-short.txt
+#and figure out who has the sent the greatest number
+#of mail messages.
+
+#The program looks for 'From ' lines and takes the second word
+#of those lines as the person who sent the mail.
+
+#The program creates a Python dictionary that maps
+#the sender's mail address to a count of the number of times
+#they appear in the file.
+
+#After the dictionary is produced, the program reads
+#through the dictionary using a maximum loop to find the
+#most prolific committer.
+
+fname = raw_input("Enter file:")
+if len(fname) < 1 : fname = "mbox-short.txt"
+fh = open(fname)
+
+counts = dict()
+
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('From '): continue
+    words = line.split()
+    print words
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+            
+for key in counts:
+        print key, counts[key]

2088b1482ad3b0c7cda6880fe980d31b0699efa8
David Lopez <lopezdp@users.noreply.github.com>
2016-08-18 10:31:46 -0400

    updating xmlextractor.py
    xml extractor complete

--- XMLExtractor.py
+++ XMLExtractor.py
@@ -3,8 +3,10 @@ import xml.etree.ElementTree as ET
 
 #prompt for link where xml data resides
 #Use this link for testing: http://python-data.dr-chuck.net/comments_42.xml
+#USe this link for actual: http://python-data.dr-chuck.net/comments_253617.xml
 
 url = raw_input('Enter URL Link: ')
+sum = 0
 
 #open url and prep for parsing
 data = urllib.urlopen(url).read()
@@ -12,12 +14,16 @@ data = urllib.urlopen(url).read()
 #read url data and convert to XML Node Tree for parsing
 comments = ET.fromstring(data)
 
-counts = comments.findall('comments/comment/count')
+#for loop to iterate through count nodes using .findall()
+for counts in comments.findall('comments/comment/count'):
+	#sum each node using counts.text method to extract value and typecast to int
+	sum = sum + int(counts.text)
 
-print len(counts)
+print sum
 
-for count in counts:
-    print 'count', count.find('count').text
+
+
+
 
 
 

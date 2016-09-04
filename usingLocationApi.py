c2d01caaed36f4a0a3af7f84878f24044a1255aa
David Lopez <lopezdp@users.noreply.github.com>
2016-09-04 13:44:45 -0400

    create usingLocationApi.py
    enter the location you may be looking for "Florida International Univarsity" and receive the google api "Place_ID" in return needs input validation and other issues

---
+++ usingLocationApi.py
@@ -0,0 +1,22 @@
+import urllib
+import json
+
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+#serviceurl = 'http://python-data.dr-chuck.net/geojson?'
+
+address = raw_input('Enter location: ')
+
+url = serviceurl + urllib.urlencode({'sensor':'false', 'address': address})
+print 'Retrieving', url
+
+data = urllib.urlopen(url).read()
+
+print 'Retrieved',len(str(data)),'characters'
+
+jsfile = json.loads(data)
+
+print json.dumps(jsfile, indent = 4)
+
+pid = jsfile['results'][0]['place_id']
+
+print "Place id: ", pid

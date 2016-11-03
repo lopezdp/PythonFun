8ab8973c34db7a7fef54e17c144e1af090589f77
David Lopez <lopezdp@users.noreply.github.com>
2016-11-03 01:35:48 -0400

    created HR.printFunction.py
    iterate up to a number N and print each number. try to solve using one line of code with map() and lambda...

---
+++ HR.printFunction.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+map(lambda x: print(x + 1, end = ''), xrange(input()))
+
+
+#n = int(raw_input().strip())
+#count = 0
+
+#for x in xrange(n):
+#	count = count + 1
+#	print (count, sep = '', end = '')
